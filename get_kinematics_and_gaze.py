c69e72c84d7120b0f42b48278ca745bf15e1fc29
Charbie <eve.charbonneau.1@umontreal.ca>
2023-04-21 09:09:04 -0400

    added other reconstructions methods

--- get_kinematics_and_gaze.py
+++ get_kinematics_and_gaze.py
@@ -5,13 +5,118 @@ The goal of this program is to get the joint and gaze angles to compare with the
 import numpy as np
 import pickle
 from IPython import embed
-import biorbd
-import bioviz
 import matplotlib.pyplot as plt
 import os
 from scipy import signal
 
-FLAG_SHOW_BIOVIZ = False # True # False
+import biorbd
+import bioviz
+from bioptim import (
+    BiorbdModel,
+    OptimalControlProgram,
+    DynamicsList,
+    DynamicsFcn,
+    BoundsList,
+    InitialGuessList,
+    ObjectiveList,
+    ObjectiveFcn,
+    Node,
+    Solver,
+    InterpolationType,
+)
+
+
+def prepare_optimal_estimation(biorbd_model_path, markers_xsens, x_init_kalman):
+
+    biorbd_model = BiorbdModel(biorbd_model_path)
+    n_shooting = markers_xsens.shape[2] - 1
+    final_time = n_shooting * 1/200
+
+    # Add objective functions
+    objective_functions = ObjectiveList()
+    objective_functions.add(
+        ObjectiveFcn.Lagrange.TRACK_MARKERS,
+        node=Node.ALL,
+        weight=100,
+        target=markers_xsens,
+    )
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="qddot_joints")
+
+    # Dynamics
+    dynamics = DynamicsList()
+    dynamics.add(DynamicsFcn.JOINTS_ACCELERATION_DRIVEN)
+
+    # Path constraint
+    x_bounds = BoundsList()
+    x_bounds.add(bounds=biorbd_model.bounds_from_ranges(["q", "qdot"]))
+
+    qddot_joints_min, qddot_joints_max, qddot_joints_init = -1000, 1000, 0
+    nb_u = biorbd_model.nb_q - biorbd_model.nb_root
+    u_bounds = BoundsList()
+    u_bounds.add([qddot_joints_min] * nb_u, [qddot_joints_max] * nb_u)
+
+    # Initial guess
+    x_init = InitialGuessList()
+    x_init.add(x_init_kalman, interpolation=InterpolationType.EACH_FRAME)
+
+    u_init = InitialGuessList()
+    u_init.add([qddot_joints_init] * nb_u)
+
+    return OptimalControlProgram(
+        biorbd_model,
+        dynamics,
+        n_shooting,
+        final_time,
+        x_init,
+        u_init,
+        x_bounds,
+        u_bounds,
+        objective_functions,
+    )
+
+def recons_kalman(num_frames, num_markers, markers_xsens, model):
+    markersOverFrames = []
+    for i in range(num_frames):
+        node_segment = []
+        for j in range(num_markers):
+            node_segment.append(biorbd.NodeSegment(markers_xsens[:, j, i].T))
+        markersOverFrames.append(node_segment)
+
+    # Create a Kalman filter structure
+    freq = 200  # Hz
+    params = biorbd.KalmanParam(freq)
+    kalman = biorbd.KalmanReconsMarkers(model, params)
+
+    # Perform the kalman filter for each frame (the first frame is much longer than the next)
+    Q = biorbd.GeneralizedCoordinates(model)
+    Qdot = biorbd.GeneralizedVelocity(model)
+    Qddot = biorbd.GeneralizedAcceleration(model)
+    q_recons = np.ndarray((model.nbQ(), len(markersOverFrames)))
+    qdot_recons = np.ndarray((model.nbQ(), len(markersOverFrames)))
+    for i, targetMarkers in enumerate(markersOverFrames):
+        kalman.reconstructFrame(model, targetMarkers, Q, Qdot, Qddot)
+        q_recons[:, i] = Q.to_array()
+        qdot_recons[:, i] = Qdot.to_array()
+    return q_recons, qdot_recons
+
+
+def recons_ik_trf(markers_xsens, model):
+    ik = biorbd.InverseKinematics(model, markers_xsens)
+    ik.solve("trf")
+    q_recons_wrapped = ik.q
+    q_recons = np.unwrap(q_recons_wrapped)
+    return q_recons
+
+def smooth_q_recons(q_recons):
+    b, a = signal.ellip(4, 0.01, 120, 0.125)  # Filter to be applied: 4th order, 0.125 normalized cutoff frequency
+    q_recons_smoothed = np.zeros(np.shape(q_recons))
+    for i in range(28):
+        q_recons_smoothed[i, :] = signal.filtfilt(b, a, q_recons[i, :], method="gust")
+    return q_recons_smoothed
+
+
+FLAG_SHOW_BIOVIZ = False  # True
+RECONSTRUCTION_METHOD = "OCP"  # "OCP", "KALMAN" or "IK_TRF"
 
 move_path = "/home/charbie/disk/Eye-tracking/Results/SoMe/831</"
 save_path = "/home/charbie/Documents/Programmation/VisionOCP/Kalman_recons/"
@@ -76,57 +181,24 @@ for filename in os.listdir(move_path):
     markers_xsens[:, 15, :] = JCS_xsens[:, 17, :]  # RightFoot_marker
     markers_xsens[:, 16, :] = JCS_xsens[:, 21, :]  # LeftFoot_marker
 
+    if RECONSTRUCTION_METHOD == "KALMAN":
+        q_recons, _ = recons_kalman(num_frames, num_markers, markers_xsens, model)
 
-    markersOverFrames = []
-    for i in range(num_frames):
-        node_segment = []
-        for j in range(num_markers):
-            node_segment.append(biorbd.NodeSegment(markers_xsens[:, j, i].T))
-        markersOverFrames.append(node_segment)
+    elif RECONSTRUCTION_METHOD == "IK_TRF":
+        q_recons = recons_ik_trf(markers_xsens, model)
 
+    elif RECONSTRUCTION_METHOD == "OCP":
+        q_init, qdot_init = recons_kalman(num_frames, num_markers, markers_xsens, model)
+        q_init_smoothed = smooth_q_recons(q_init)
+        x_init_kalman = np.vstack((q_init_smoothed, qdot_init))
 
-    # Create a Kalman filter structure
-    freq = 200  # Hz
-    params = biorbd.KalmanParam(freq)
-    kalman = biorbd.KalmanReconsMarkers(model, params)
-
-    # Perform the kalman filter for each frame (the first frame is much longer than the next)
-    Q = biorbd.GeneralizedCoordinates(model)
-    Qdot = biorbd.GeneralizedVelocity(model)
-    Qddot = biorbd.GeneralizedAcceleration(model)
-    q_recons = np.ndarray((model.nbQ(), len(markersOverFrames)))
-    for i, targetMarkers in enumerate(markersOverFrames):
-        kalman.reconstructFrame(model, targetMarkers, Q, Qdot, Qddot)
-        q_recons[:, i] = Q.to_array()
-
-
-    # # create a 3D plot of the markers
-    # colors = plt.cm.get_cmap('viridis', num_markers)
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    # ax.scatter(markers_xsens[0, 0, 0], markers_xsens[1, 0, 0], markers_xsens[2, 0, 0], c=colors(0/16), marker='o', label='Pelvis_marker')
-    # ax.scatter(markers_xsens[0, 1, 0], markers_xsens[1, 1, 0], markers_xsens[2, 1, 0], c=colors(1/16), marker='o', label='T12_marker')
-    # ax.scatter(markers_xsens[0, 2, 0], markers_xsens[1, 2, 0], markers_xsens[2, 2, 0], c=colors(2/16), marker='o', label='C1_marker')
-    # ax.scatter(markers_xsens[0, 3, 0], markers_xsens[1, 3, 0], markers_xsens[2, 3, 0], c=colors(3/16), marker='o', label='eyes_vect_start')
-    # ax.scatter(markers_xsens[0, 4, 0], markers_xsens[1, 4, 0], markers_xsens[2, 4, 0], c=colors(4/16), marker='o', label='eyes_vect_end')
-    # ax.scatter(markers_xsens[0, 5, 0], markers_xsens[1, 5, 0], markers_xsens[2, 5, 0], c=colors(5/16), marker='o', label='RightUpperArm_marker')
-    # ax.scatter(markers_xsens[0, 6, 0], markers_xsens[1, 6, 0], markers_xsens[2, 6, 0], c=colors(6/16), marker='o', label='RightForeArm_marker')
-    # ax.scatter(markers_xsens[0, 7, 0], markers_xsens[1, 7, 0], markers_xsens[2, 7, 0], c=colors(7/16), marker='o', label='RightHand_marker')
-    # ax.scatter(markers_xsens[0, 8, 0], markers_xsens[1, 8, 0], markers_xsens[2, 8, 0], c=colors(8/16), marker='o', label='LeftUpperArm_marker')
-    # ax.scatter(markers_xsens[0, 9, 0], markers_xsens[1, 9, 0], markers_xsens[2, 9, 0], c=colors(9/16), marker='o', label='LeftForeArm_marker')
-    # ax.scatter(markers_xsens[0, 10, 0], markers_xsens[1, 10, 0], markers_xsens[2, 10, 0], c=colors(10/16), marker='o', label='RightHand_marker')
-    # ax.scatter(markers_xsens[0, 11, 0], markers_xsens[1, 11, 0], markers_xsens[2, 11, 0], c=colors(11/16), marker='o', label='RightUpperLeg_marker')
-    # ax.scatter(markers_xsens[0, 12, 0], markers_xsens[1, 12, 0], markers_xsens[2, 12, 0], c=colors(12/16), marker='o', label='LeftUpperLeg_marker')
-    # ax.scatter(markers_xsens[0, 13, 0], markers_xsens[1, 13, 0], markers_xsens[2, 13, 0], c=colors(13/16), marker='o', label='RightLowerLeg_marker')
-    # ax.scatter(markers_xsens[0, 14, 0], markers_xsens[1, 14, 0], markers_xsens[2, 14, 0], c=colors(14/16), marker='o', label='LeftLowerLeg_marker')
-    # ax.scatter(markers_xsens[0, 15, 0], markers_xsens[1, 15, 0], markers_xsens[2, 15, 0], c=colors(15/16), marker='o', label='RightFoot_marker')
-    # ax.scatter(markers_xsens[0, 16, 0], markers_xsens[1, 16, 0], markers_xsens[2, 16, 0], c=colors(16/16), marker='o', label='LeftFoot_marker')
-    # ax.set_xlabel('X [m]')
-    # ax.set_ylabel('Y [m]')
-    # ax.set_zlabel('Z [m]')
-    # ax.legend()
-    # plt.savefig("markers_and_joints.png")
-    # # plt.show()
+        ocp = prepare_optimal_estimation(biorbd_model_path, markers_xsens, x_init_kalman)
+        solver = Solver.IPOPT(show_online_optim=True, show_options=dict(show_bounds=True))
+        solver.set_linear_solver("ma57")
+        solver.set_maximum_iterations(10000)
+        solver.set_convergence_tolerance(1e-4)
+        sol = ocp.solve(solver)
+        q_recons = sol.states["q"]
 
     if FLAG_SHOW_BIOVIZ:
         print(filename)
@@ -135,11 +207,7 @@ for filename in os.listdir(move_path):
         b.load_experimental_markers(markers_xsens)
         b.exec()
 
-    # Smooth the q_recons
-    b, a = signal.ellip(4, 0.01, 120, 0.125)  # Filter to be applied: 4th order, 0.125 normalized cutoff frequency
-    q_recons_smoothed = np.zeros(np.shape(q_recons))
-    for i in range(28):
-        q_recons_smoothed[i, :] = signal.filtfilt(b, a, q_recons[i, :], method="gust")
+
 
     save_name = save_path + filename[:-24] + 'kalman_recons'
     fig, axs = plt.subplots(7, 4, figsize=(20, 10))
