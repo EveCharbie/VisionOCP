355d5f8db869eb2dae5ee44e1074c8af619e0c41
Charbie <eve.charbonneau.1@umontreal.ca>
2023-07-14 16:27:14 +0200

    Version compatible with 15/05/23

--- TechOpt42.py
+++ TechOpt42.py
@@ -12,12 +12,11 @@ import casadi as cas
 import IPython
 import time
 import sys
-sys.path.append("/home/charbie/Documents/Programmation/BiorbdOptim")
+
 from bioptim import (
     OptimalControlProgram,
     DynamicsList,
     DynamicsFcn,
-    PenaltyNode,
     ObjectiveList,
     ObjectiveFcn,
     BoundsList,
@@ -30,26 +29,27 @@ from bioptim import (
     CostType,
     ConstraintList,
     ConstraintFcn,
-    PenaltyNodeList,
+    PenaltyController,
     BiorbdModel,
     Shooting,
     SolutionIntegrator,
 )
 
-def custom_trampoline_bed_in_peripheral_vision(all_pn: PenaltyNodeList) -> cas.MX:
+def custom_trampoline_bed_in_peripheral_vision(controller: PenaltyController) -> cas.MX:
     """
     This function aims to encourage the avatar to keep the trampoline bed in his peripheral vision.
     It is done by discretizing the vision cone into vectors and determining if the vector projection of the gaze are inside the trampoline bed.
     """
 
+    q = controller
     a = 1.07  # Trampoline with/2
     b = 2.14  # Trampoline length/2
     n = 6  # order of the polynomial for the trampoline bed rectangle equation
 
     # Get the gaze vector
-    eyes_vect_start_marker_idx = all_pn.nlp.model.marker_index(f'eyes_vect_start')
-    eyes_vect_end_marker_idx = all_pn.nlp.model.marker_index(f'eyes_vect_end')
-    gaze_vector = all_pn.nlp.model.markers(all_pn.nlp.states["q"].mx)[eyes_vect_end_marker_idx] - all_pn.nlp.model.markers(all_pn.nlp.states["q"].mx)[eyes_vect_start_marker_idx]
+    eyes_vect_start_marker_idx = controller.model.marker_index(f'eyes_vect_start')
+    eyes_vect_end_marker_idx = controller.model.marker_index(f'eyes_vect_end')
+    gaze_vector = controller.model.markers(controller.states["q"].mx)[eyes_vect_end_marker_idx] - controller.model.markers(controller.states["q"].mx)[eyes_vect_start_marker_idx]
 
     point_in_the_plane = np.array([1, 2, -0.83])
     vector_normal_to_the_plane = np.array([0, 0, 1])
@@ -58,9 +58,9 @@ def custom_trampoline_bed_in_peripheral_vision(all_pn: PenaltyNodeList) -> cas.M
         for i_th in range(10):
 
             # Get this vector from the vision cone
-            marker_idx = all_pn.nlp.model.marker_index(f'cone_approx_{i_r}_{i_th}')
-            vector_origin = all_pn.nlp.model.markers(all_pn.nlp.states["q"].mx)[eyes_vect_start_marker_idx]
-            vector_end = all_pn.nlp.model.markers(all_pn.nlp.states["q"].mx)[marker_idx]
+            marker_idx = controller.model.marker_index(f'cone_approx_{i_r}_{i_th}')
+            vector_origin = controller.model.markers(controller.states["q"].mx)[eyes_vect_start_marker_idx]
+            vector_end = controller.model.markers(controller.states["q"].mx)[marker_idx]
             vector = vector_end - vector_origin
 
             # Get the intersection between the vector and the trampoline plane
@@ -81,7 +81,7 @@ def custom_trampoline_bed_in_peripheral_vision(all_pn: PenaltyNodeList) -> cas.M
     val = cas.if_else(gaze_vector[2] > -0.01, 2*10*11,
                 cas.if_else(cas.fabs(gaze_vector[0]/gaze_vector[2]) > np.tan(3*np.pi/8), 2*10*11,
                             cas.if_else(cas.fabs(gaze_vector[1]/gaze_vector[2]) > np.tan(3*np.pi/8), 2*10*11, obj)))
-    out = all_pn.nlp.mx_to_cx("peripheral_vision", val, all_pn.nlp.states["q"])
+    out = controller.mx_to_cx("peripheral_vision", val, controller.states["q"])
 
     return out
 
@@ -455,6 +455,7 @@ def prepare_ocp(
         objective_functions,
         ode_solver=ode_solver,
         n_threads=n_threads,
+        assume_phase_dynamics=True,
     )
 
 
@@ -475,11 +476,18 @@ def main():
     ocp = prepare_ocp(biorbd_model_path, n_shooting=n_shooting, num_twists=num_twists, n_threads=7, WITH_VISUAL_CRITERIA=WITH_VISUAL_CRITERIA)
     # ocp.add_plot_penalty(CostType.ALL)
 
-    solver = Solver.IPOPT(show_online_optim=True, show_options=dict(show_bounds=True))
+    # solver = Solver.IPOPT(show_online_optim=True, show_options=dict(show_bounds=True))
+    solver = Solver.IPOPT(show_online_optim=False)
     solver.set_linear_solver("ma57")
-    solver.set_maximum_iterations(10000)
+    solver.set_maximum_iterations(10)
     solver.set_convergence_tolerance(1e-4)
+
+    tic = time.time()
     sol = ocp.solve(solver)
+    toc = time.time() - tic
+    print(toc)  
+    # Before: 3.6.2: 74.02791595458984s 
+    # Before: 3.6.3: 81s
 
     timestamp = time.strftime("%Y-%m-%d-%H%M")
     name = biorbd_model_path.split("/")[-1].removesuffix(".bioMod")
