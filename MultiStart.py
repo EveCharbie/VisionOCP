19391754549479447828529f402208acc204d9dd
Charbie <eve.charbonneau.1@umontreal.ca>
2023-11-07 12:28:34 -0500

    multi-start complete

--- MultiStart.py
+++ MultiStart.py
@@ -8,15 +8,15 @@ import TechOpt42 as ocp_module_42
 import TechOpt831 as ocp_module_831
 
 vision_weights = [
-    # 0.0,
-    # 0.25,
-    # 0.5,
-    # 0.75,
-    # 1.0,
+    0.0,
+    0.25,
+    0.5,
+    0.75,
+    1.0,
     1.25,
-    # 1.5,
-    # 1.75,
-    # 2.0,
+    1.5,
+    1.75,
+    2.0,
 ]
 
 for weight in vision_weights:
@@ -24,7 +24,7 @@ for weight in vision_weights:
         WITH_VISUAL_CRITERIA = False
     else:
         WITH_VISUAL_CRITERIA = True
-    # ocp_module_42.main(WITH_VISUAL_CRITERIA=WITH_VISUAL_CRITERIA, visual_weight=weight)
+    ocp_module_42.main(WITH_VISUAL_CRITERIA=WITH_VISUAL_CRITERIA, visual_weight=weight)
     ocp_module_831.main(WITH_VISUAL_CRITERIA=WITH_VISUAL_CRITERIA, visual_weight=weight)
     print(f"\n\n\n\n I am done with {weight} :) \n\n\n\n")
 
