441121f936d520da91f3c7e5c217b6c7e8dc029d
Charbie <eve.charbonneau.1@umontreal.ca>
2024-01-14 14:54:24 -0500

    added rating results from coaches and judges

--- compile_coaches_judges_ratings.py
+++ compile_coaches_judges_ratings.py
@@ -9,50 +9,96 @@ colors = ['#151F31', '#103778', '#0593A2', '#FF7A48', '#E3371E', '#E5003F']
 
 ratings = {
     "Stephen": {
-        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p0_CVG": [2, 3, 2, 2, 4, 2],
-        "a62d4691_0_0-45_796__42__1": [5, 5, 5, 5, 5, 5],
-        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p0_CVG": [2, 2, 2, 2, 4, 2],
-        "a62d4691_0_0-45_796__42__0": [4, 4, 4, 2, 3, 2],
-        "87972c15_0_0-105_114__831__1": [2, 2, 3, 1, 3, 2],
-        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p25_CVG": [5, 5, 5, 5, 5, 5],
-        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-2p0_CVG": [2, 3, 3, 2, 3, 2],
-        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p75_CVG": [4, 4, 4, 2, 4, 4],
-        "SoMe_42_without_mesh-(100_40)-0p0_CVG": [4, 4, 4, 4, 4, 4],
-        "a62d4691_0_0-45_796__42__3": [2, 4, 4, 2, 4, 2],
-        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p75_CVG": [4, 2, 3, 2, 4, 2],
-        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-2p0_CVG": [4, 4, 4, 2, 4, 3],
-        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p5_CVG": [4, 4, 4, 4, 4, 4],
-        "87972c15_0_0-105_114__831__0": [2, 3, 3, 1, 3, 1],
-        "SoMe_without_mesh_831-(40_40_40_40_40_40)-2023-0p0_CVG": [5, 5, 5, 4, 5, 5],
-        "87972c15_0_0-105_114__831__2": [4, 4, 4, 2, 4, 2],
-        "a62d4691_0_0-45_796__42__2": [3, 4, 4, 2, 4, 2],
-        "c2a19850_0_0-113_083__831__0": [2, 3, 3, 2, 3, 1],
-        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p5_CVG": [3, 4, 4, 2, 4, 2],
-        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p25_CVG": [4, 4, 4, 4, 4, 4],
-        "caccfb24_0_0-48_286__42__1": [4, 4, 4, 4, 4, 4],
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p0_CVG": [2, 3, 2, 2, 4, 2],  # A
+        "a62d4691_0_0-45_796__42__1": [5, 5, 5, 5, 5, 5],  # B
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p0_CVG": [2, 2, 2, 2, 4, 2],  # C
+        "a62d4691_0_0-45_796__42__0": [4, 4, 4, 2, 3, 2],  # D
+        "87972c15_0_0-105_114__831__1": [2, 2, 3, 1, 3, 2],  # E
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p25_CVG": [5, 5, 5, 5, 5, 5],  # F
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-2p0_CVG": [2, 3, 3, 2, 3, 2],  # G
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p75_CVG": [4, 4, 4, 2, 4, 4],  # H
+        "SoMe_42_without_mesh-(100_40)-0p0_CVG": [4, 4, 4, 4, 4, 4],  # I
+        "a62d4691_0_0-45_796__42__3": [2, 4, 4, 2, 4, 2],  # J
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p75_CVG": [4, 2, 3, 2, 4, 2],  # K
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-2p0_CVG": [4, 4, 4, 2, 4, 3],  # L
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p5_CVG": [4, 4, 4, 4, 4, 4],  # M
+        "87972c15_0_0-105_114__831__0": [2, 3, 3, 1, 3, 1],  # N
+        "SoMe_without_mesh_831-(40_40_40_40_40_40)-2023-0p0_CVG": [5, 5, 5, 4, 5, 5],  # O
+        "87972c15_0_0-105_114__831__2": [4, 4, 4, 2, 4, 2],  # P
+        "a62d4691_0_0-45_796__42__2": [3, 4, 4, 2, 4, 2],  # Q
+        "c2a19850_0_0-113_083__831__0": [2, 3, 3, 2, 3, 1],  # R
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p5_CVG": [3, 4, 4, 2, 4, 2],  # S
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p25_CVG": [4, 4, 4, 4, 4, 4],  # T
+        "caccfb24_0_0-48_286__42__1": [4, 4, 4, 4, 4, 4],  # U
     },
-    "Eve": {  # fake data to build the graph
-        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p0_CVG": [2, 3, 2, 2, 4, 2],
-        "a62d4691_0_0-45_796__42__1": [5, 5, 5, 5, 5, 5],
-        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p0_CVG": [2, 2, 2, 2, 4, 2],
-        "a62d4691_0_0-45_796__42__0": [4, 4, 4, 2, 3, 2],
-        "87972c15_0_0-105_114__831__1": [2, 2, 3, 1, 3, 2],
-        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p25_CVG": [5, 5, 5, 5, 5, 5],
-        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-2p0_CVG": [2, 3, 3, 2, 3, 2],
-        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p75_CVG": [4, 4, 4, 2, 4, 4],
-        "SoMe_42_without_mesh-(100_40)-0p0_CVG": [4, 4, 4, 4, 4, 4],
-        "a62d4691_0_0-45_796__42__3": [2, 4, 4, 2, 4, 2],
-        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p75_CVG": [4, 2, 3, 2, 4, 2],
-        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-2p0_CVG": [4, 4, 4, 2, 4, 3],
-        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p5_CVG": [4, 4, 4, 4, 4, 4],
-        "87972c15_0_0-105_114__831__0": [2, 3, 3, 1, 3, 1],
-        "SoMe_without_mesh_831-(40_40_40_40_40_40)-2023-0p0_CVG": [5, 5, 5, 4, 5, 5],
-        "87972c15_0_0-105_114__831__2": [4, 4, 4, 2, 4, 2],
-        "a62d4691_0_0-45_796__42__2": [3, 4, 4, 2, 4, 2],
-        "c2a19850_0_0-113_083__831__0": [2, 3, 3, 2, 3, 1],
-        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p5_CVG": [3, 4, 4, 2, 4, 2],
-        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p25_CVG": [4, 4, 4, 4, 4, 4],
-        "caccfb24_0_0-48_286__42__1": [4, 4, 4, 4, 4, 4],
+    "Antoine": {
+        "a62d4691_0_0-45_796__42__0": [5, 5, 5, 4, 5, 5],  # A
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p75_CVG": [5, 5, 4, 2, 5, 2],  # B
+        "a62d4691_0_0-45_796__42__3": [5, 5, 5, 5, 5, 5],  # C
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p0_CVG": [4, 3, 2, 1, 5, 2],  # D
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p5_CVG": [5, 2, 1, 1, 4, 1],  # E
+        "87972c15_0_0-105_114__831__2": [4, 5, 5, 3, 5, 4],  # F
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p25_CVG": [5, 3, 2, 1, 5, 1],  # G
+        "caccfb24_0_0-48_286__42__1": [5, 5, 5, 5, 5, 5],  # H
+        "a62d4691_0_0-45_796__42__1": [5, 5, 5, 5, 4, 5],  # I
+        "87972c15_0_0-105_114__831__1": [1, 4, 4, 1, 5, 2],  # J
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-2p0_CVG": [5, 4, 2, 1, 4, 2],  # K
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p75_CVG": [5, 3, 1, 1, 4, 1],  # L
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p0_CVG": [5, 3, 2, 1, 4, 1],  # M
+        "87972c15_0_0-105_114__831__0": [1, 5, 5, 1, 5, 1],  # N
+        "a62d4691_0_0-45_796__42__2": [4, 5, 5, 5, 5, 4],  # O
+        "SoMe_without_mesh_831-(40_40_40_40_40_40)-2023-0p0_CVG": [5, 5, 2, 1, 4, 1],  # P
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-2p0_CVG": [3, 4, 2, 1, 5, 1],  # Q
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p25_CVG": [4, 4, 2, 1, 5, 1],  # R
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p5_CVG": [4, 4, 3, 2, 5, 2],  # S
+        "c2a19850_0_0-113_083__831__0": [2, 4, 4, 1, 5, 1],  # T
+        "SoMe_42_without_mesh-(100_40)-0p0_CVG": [5, 4, 3, 2, 5, 2],  # U
+    },
+    "Marty": {
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-2p0_CVG": [4, 2, 2, 2, 4, 2],  # A
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p0_CVG": [4, 2, 2, 2, 4, 2],  # B
+        "a62d4691_0_0-45_796__42__1": [4, 4, 4, 4, 4, 4],  # C
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p5_CVG": [4, 2, 2, 2, 4, 2],  # D
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p75_CVG": [4, 2, 2, 2, 4, 2],  # E
+        "87972c15_0_0-105_114__831__1": [4, 4, 4, 2, 4, 3],  # F
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p75_CVG": [3, 3, 4, 2, 4, 2],  # G
+        "SoMe_42_without_mesh-(100_40)-0p0_CVG": [3, 2, 4, 4, 2, 2],  # H
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-2p0_CVG": [3, 3, 4, 2, 4, 2],  # I
+        "caccfb24_0_0-48_286__42__1": [4, 4, 4, 4, 4, 4],  # J
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p25_CVG": [3, 4, 3, 4, 4, 3],  # K
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p25_CVG": [4, 2, 2, 2, 4, 2],  # L
+        "a62d4691_0_0-45_796__42__0": [4, 4, 4, 4, 4, 4],  # M
+        "c2a19850_0_0-113_083__831__0": [4, 4, 4, 2, 4, 3],  # N
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p0_CVG": [3, 3, 4, 2, 4, 2],  # O
+        "SoMe_without_mesh_831-(40_40_40_40_40_40)-2023-0p0_CVG": [3, 2, 2, 2, 2.5, 2],  # P
+        "a62d4691_0_0-45_796__42__3": [4, 4, 4, 4, 4, 4],  # Q
+        "a62d4691_0_0-45_796__42__2": [4, 4, 4, 4, 4, 4],  # R
+        "87972c15_0_0-105_114__831__2": [4, 4, 4, 2, 4, 2],  # S
+        "87972c15_0_0-105_114__831__0": [4, 4, 4, 2, 4, 2],  # T
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p5_CVG": [4, 4, 4, 4, 4, 4],  # U
+    },
+    "Karina": {
+        "SoMe_42_without_mesh-(100_40)-0p0_CVG": [5, 5, 5, 5, 5, 5],  # A
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-2p0_CVG": [5, 4, 4, 2, 4, 2],  # B
+        "a62d4691_0_0-45_796__42__0": [5, 5, 5, 4, 5, 4],  # C
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p0_CVG": [2, 3, 3, 2, 3, 2],  # D
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p75_CVG": [3, 2, 3, 3, 3, 2],  # E
+        "87972c15_0_0-105_114__831__1": [2, 4, 4, 3, 3, 3],  # F
+        "a62d4691_0_0-45_796__42__2": [4, 4, 4, 4, 4, 3],  # G
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p25_CVG": [4, 3, 4, 2, 4, 2],  # H
+        "87972c15_0_0-105_114__831__0": [2, 3, 3, 2, 2, 2],  # I
+        "87972c15_0_0-105_114__831__2": [3, 4, 4, 3, 4, 3],  # J
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-2p0_CVG": [4, 2, 3, 2, 2, 1],  # K
+        "a62d4691_0_0-45_796__42__1": [4, 4, 4, 4, 4, 4],  # L
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p75_CVG": [4, 3, 3, 2, 4, 3],  # M
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p5_CVG": [3, 2, 2, 2, 3, 1],  # N
+        "SoMe_without_mesh_831-(40_40_40_40_40_40)-2023-0p0_CVG": [3, 2, 3, 1, 3, 1],  # O
+        "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p0_CVG": [2, 2, 2, 2, 3, 2],  # P
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p5_CVG": [2, 2, 2, 3, 2, 1],  # Q
+        "a62d4691_0_0-45_796__42__3": [3, 4, 4, 3, 4, 3],  # R
+        "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p25_CVG": [4, 4, 4, 4, 3, 2],  # S
+        "c2a19850_0_0-113_083__831__0": [2, 3, 3, 2, 3, 2],  # T
+        "caccfb24_0_0-48_286__42__1": [3, 4, 4, 4, 4, 4],  # U
     }
 }
 
@@ -117,6 +163,7 @@ for i_trial, trial in enumerate(names_42_simulations):
 axs[0].set_xticks(x)
 axs[0].set_xticklabels(conditions_simulations)
 axs[0].set_xlim(-bar_width, 7 * bar_width*6)
+axs[0].set_ylim(0, 20)
 
 # Create a bar plot for real 42/
 for i_trial, trial in enumerate(names_42_real):
@@ -136,6 +183,7 @@ for i_trial, trial in enumerate(names_42_real):
 axs[1].set_xticks(x)
 axs[1].set_xticklabels(conditions_reel)
 axs[1].set_xlim(-bar_width, 7 * bar_width*6)
+axs[1].set_ylim(0, 20)
 
 # Create a bar plot for simulated 831<
 for i_trial, trial in enumerate(names_831_simulations):
@@ -155,6 +203,7 @@ for i_trial, trial in enumerate(names_831_simulations):
 axs[2].set_xticks(x)
 axs[2].set_xticklabels(conditions_simulations)
 axs[2].set_xlim(-bar_width, 7 * bar_width*6)
+axs[2].set_ylim(0, 20)
 
 # Create a bar plot for real 831<
 for i_trial, trial in enumerate(names_831_real):
@@ -174,11 +223,11 @@ for i_trial, trial in enumerate(names_831_real):
 axs[3].set_xticks(x)
 axs[3].set_xticklabels(conditions_reel)
 axs[3].set_xlim(-bar_width, 7 * bar_width*6)
+axs[3].set_ylim(0, 20)
 
 for i_criteria in range(len(criteria)):
     axs[0].plot(-1, -1, color=colors[i_criteria], label=criteria[i_criteria])
 axs[0].legend(bbox_to_anchor=(0.5, 2.25), loc='upper center')
-axs[0].set_ylim(0, 10)
 
 plt.subplots_adjust(top=0.80, bottom=0.05, left=0.05, right=0.95)
 plt.savefig('coaches_ratings', format='eps', dpi=300)
@@ -187,49 +236,49 @@ plt.show()
 
 ### ------------- Judges ratings -------------- ###
 
-ratings = {"Stephan": {"A": [1],
-                       "B": [1],
-                       "C": [1],
-                       "D": [1],
-                       "E": [1],
-                       "F": [1],
-                       "G": [1],
-                       "H": [1],
-                       "I": [1],
-                       "J": [1],
-                       "K": [1],
-                       "L": [1],
-                       "M": [1],
-                       "N": [1],
-                       "O": [1],
-                       "P": [1],
-                       "Q": [1],
-                       "R": [1],
-                       "S": [1],
-                       "T": [1],
-                       "U": [1],
+ratings = {"Stephan": {"caccfb24_0_0-48_286__42__1": [0.1],  # A
+                       "a62d4691_0_0-45_796__42__0": [0.2],  # B
+                       "a62d4691_0_0-45_796__42__1": [0.2],  # C
+                       "SoMe_without_mesh_831-(40_40_40_40_40_40)-2023-0p0_CVG": [0.1],  # D
+                       "c2a19850_0_0-113_083__831__0": [0.4],  # E
+                       "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-2p0_CVG": [0.1],  # F
+                       "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p5_CVG": [0.1],  # G
+                       "87972c15_0_0-105_114__831__1": [0.4],  # H
+                       "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p0_CVG": [0.1],  # I
+                       "a62d4691_0_0-45_796__42__3": [0.1],  # J
+                       "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p75_CVG": [0.2],  # K
+                       "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p5_CVG": [0.1],  # L
+                       "a62d4691_0_0-45_796__42__2": [0.2],  # M
+                       "87972c15_0_0-105_114__831__2": [0.4],  # N
+                       "SoMe_42_with_visual_criteria_without_mesh-(100_40)-2p0_CVG": [0.2],  # O
+                       "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p0_CVG": [0.1],  # P
+                       "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p75_CVG": [0.2],  # Q
+                       "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p25_CVG": [0.1],  # R
+                       "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p25_CVG": [0.1],  # S
+                       "87972c15_0_0-105_114__831__0": [0.4],  # T
+                       "SoMe_42_without_mesh-(100_40)-0p0_CVG": [0.3],  # U
                        },
-            "Julie": {"A": [1],
-                      "B": [1],
-                      "C": [1],
-                      "D": [1],
-                      "E": [1],
-                      "F": [1],
-                      "G": [1],
-                      "H": [1],
-                      "I": [1],
-                      "J": [1],
-                      "K": [1],
-                      "L": [1],
-                      "M": [1],
-                      "N": [1],
-                      "O": [1],
-                      "P": [1],
-                      "Q": [1],
-                      "R": [1],
-                      "S": [1],
-                      "T": [1],
-                      "U": [1],
+            "Julie": {"SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p25_CVG": [0.1],  # A
+                      "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p75_CVG": [0.1],  # B
+                      "87972c15_0_0-105_114__831__1": [0.4],  # C
+                      "SoMe_42_without_mesh-(100_40)-0p0_CVG": [0.1],  # D
+                      "87972c15_0_0-105_114__831__2": [0.4],  # E
+                      "a62d4691_0_0-45_796__42__2": [0.2],  # F
+                      "c2a19850_0_0-113_083__831__0": [0.5],  # G
+                      "SoMe_42_with_visual_criteria_without_mesh-(100_40)-2p0_CVG": [0.1],  # H
+                      "a62d4691_0_0-45_796__42__1": [0.3],  # I
+                      "a62d4691_0_0-45_796__42__0": [0.3],  # J
+                      "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p5_CVG": [0.1],  # K
+                      "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p0_CVG": [0.1],  # L
+                      "SoMe_without_mesh_831-(40_40_40_40_40_40)-2023-0p0_CVG": [0.2],  # M
+                      "SoMe_42_with_visual_criteria_without_mesh-(100_40)-0p5_CVG": [0.1],  # N
+                      "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-2p0_CVG": [0.2],  # O
+                      "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-0p25_CVG": [0.1],  # P
+                      "87972c15_0_0-105_114__831__0": [0.5],  # Q
+                      "caccfb24_0_0-48_286__42__1": [0.3],  # R
+                      "a62d4691_0_0-45_796__42__3": [0.3],  # S
+                      "SoMe_42_with_visual_criteria_without_mesh-(100_40)-1p75_CVG": [0.1],  # T
+                      "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-1p0_CVG": [0.2],  # U
                 },
            }
 
@@ -255,6 +304,7 @@ for i_trial, trial in enumerate(names_42_simulations):
 axs[0].set_xticks(x)
 axs[0].set_xticklabels(conditions_simulations)
 axs[0].set_xlim(-bar_width, 7 * bar_width*6)
+axs[0].set_ylim(0, 1)
 
 # Create a bar plot for real 42/
 for i_trial, trial in enumerate(names_42_real):
@@ -273,6 +323,7 @@ for i_trial, trial in enumerate(names_42_real):
 axs[1].set_xticks(x)
 axs[1].set_xticklabels(conditions_reel)
 axs[1].set_xlim(-bar_width, 7 * bar_width*6)
+axs[1].set_ylim(0, 1)
 
 # Create a bar plot for simulated 831<
 for i_trial, trial in enumerate(names_831_simulations):
@@ -291,6 +342,7 @@ for i_trial, trial in enumerate(names_831_simulations):
 axs[2].set_xticks(x)
 axs[2].set_xticklabels(conditions_simulations)
 axs[2].set_xlim(-bar_width, 7 * bar_width*6)
+axs[2].set_ylim(0, 1)
 
 # Create a bar plot for real 831<
 for i_trial, trial in enumerate(names_831_real):
@@ -309,7 +361,7 @@ for i_trial, trial in enumerate(names_831_real):
 axs[3].set_xticks(x)
 axs[3].set_xticklabels(conditions_reel)
 axs[3].set_xlim(-bar_width, 7 * bar_width*6)
-axs[0].set_ylim(0, 10)
+axs[3].set_ylim(0, 1)
 
 plt.savefig('judges_ratings', format='eps', dpi=300)
 plt.show()
