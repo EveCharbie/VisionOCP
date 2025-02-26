d6ff94fc0e2e15df30d5f9b35657b22a3dad4073
Charbie <eve.charbonneau.1@umontreal.ca>
2025-02-26 12:10:13 +0400

    animate technique update

--- animate_results.py
+++ animate_results.py
@@ -3,128 +3,196 @@ The goal of this program is to show an animation of the solution of the optimal 
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 import pickle
-import IPython
-import time
 
 import bioviz
-import bioptim
 import biorbd
 
-WITH_VISUAL_CRITERIA = False
-ACROBATICS = "831"
 
-if ACROBATICS == "831":
-    if WITH_VISUAL_CRITERIA:
-        biorbd_model_path = "models/SoMe_with_visual_criteria.bioMod"
-    else:
-        biorbd_model_path = "models/SoMe.bioMod"
-    n_shooting = (40, 100, 100, 100, 40)
-    num_twists = 1
-    name = "SoMe"
-elif ACROBATICS == "42":
-    if WITH_VISUAL_CRITERIA:
-        biorbd_model_path = "models/SoMe_42_with_visual_criteria.bioMod"
-    else:
-        biorbd_model_path = "models/SoMe_42.bioMod"
-    n_shooting = (100, 40)
-    num_twists = 1
-    name = "SoMe"
+def combine_real_time(interpolated_states_with_visual_criteria, interpolated_states):
+    qs_combined_real_time = None
+    for i in range(len(interpolated_states_with_visual_criteria)):
+        # Make qs the right size to be concatenated (removing one frame at the end is necessary)
+        q_with_visual_criteria = interpolated_states_with_visual_criteria[i]["q"]
+        q = interpolated_states[i]["q"]
+        if q_with_visual_criteria.shape[1] > q.shape[1]:
+            q_with_visual_criteria = q_with_visual_criteria[:, :q.shape[1]]
+        elif q_with_visual_criteria.shape[1] < q.shape[1]:
+            q = q[:, :q_with_visual_criteria.shape[1]]
+        # Combining them into one matrix
+        if qs_combined_real_time is None:
+            qs_combined_real_time = np.vstack((q_with_visual_criteria, q))
+        else:
+            qs_combined_real_time = np.hstack((qs_combined_real_time,
+                                               np.vstack((q_with_visual_criteria, q))))
+    return qs_combined_real_time
 
-file_name = "SoMe_without_mesh_831-(40_40_40_40_40_40)-2023-10-26-1040.pkl"  # Good 831<
-# file_name = "SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-2023-10-25-1426.pkl"  # Good 831< with visual criteria
-# file_name = "SoMe_42_without_mesh-(100_40)-2023-10-20-1652.pkl"  # Good 42/
-# file_name = "SoMe_42_with_visual_criteria_without_mesh-(100_40)-2023-10-20-1631.pkl"  # Good 42/ with visual criteria
 
-with open("Solutions/" + file_name, "rb") as f:
+# Models
+biorbd_model_path_831_with_visual_criteria = "models/SoMe_with_visual_criteria.bioMod"
+model_831_with_visual_criteria = biorbd.Model(biorbd_model_path_831_with_visual_criteria)
+biorbd_model_path_831 = "models/SoMe.bioMod"
+model_831 = biorbd.Model(biorbd_model_path_831)
+biorbd_model_path_831_both = "models/SoMe_with_and_without_visual_criteria.bioMod"
+n_shooting_831 = (40, 40, 40, 40, 40, 40)
+biorbd_model_path_42_with_visual_criteria = "models/SoMe_42_with_visual_criteria.bioMod"
+model_42_with_visual_criteria = biorbd.Model(biorbd_model_path_42_with_visual_criteria)
+biorbd_model_path_42 = "models/SoMe_42.bioMod"
+model_42 = biorbd.Model(biorbd_model_path_42)
+biorbd_model_path_42_both = "models/SoMe_42_with_and_without_visual_criteria.bioMod"
+n_shooting_42 = (100, 40)
+
+
+# Paths to techniques
+file_name_831 = "Solutions/q_SoMe_without_mesh_831-(40_40_40_40_40_40)-2023-11-13-1528-0p0_CVG.pkl"  # Good 831<
+file_name_831_with_visual_criteria = "Solutions/q_SoMe_with_visual_criteria_without_mesh_831-(40_40_40_40_40_40)-2023-11-07-2257-0p25_CVG.pkl"  # Good 831< with visual criteria
+file_name_42 = "Solutions/q_SoMe_42_without_mesh-(100_40)-2023-11-01-0932-0p0_CVG.pkl"  # Good 42/
+file_name_42_with_visual_criteria = "Solutions/q_SoMe_42_with_visual_criteria_without_mesh-(100_40)-2023-11-01-0942-0p25_CVG.pkl"   # Good 42/ with visual criteria
+
+
+# ---------------------------------------- Load data ---------------------------------------- #
+
+with open(file_name_42, "rb") as f:
     data = pickle.load(f)
-    sol = data[0]
-    q_per_phase = data[1]
-    qs = data[2]
-    qdots = data[3]
-    qddots = data[4]
-    time_parameters = data[5]
-    q_reintegrated = data[6]
-    qdot_reintegrated = data[7]
-    time_vector = data[8]
-    interpolated_states = data[9]
+    q_per_phase_42 = data["q_per_phase"]
+    qs_42 = data["qs"]
+    qdots_42 = data["qdots"]
+    qddots_42 = data["qddots"]
+    time_parameters_42 = data["time_parameters"]
+    q_reintegrated_42 = data["q_reintegrated"]
+    qdot_reintegrated_42 = data["qdot_reintegrated"]
+    time_vector_42 = data["time_vector"]
+    interpolated_states_42 = data["interpolated_states"]
+
+with open(file_name_42_with_visual_criteria, "rb") as f:
+    data = pickle.load(f)
+    q_per_phase_42_with_visual_criteria = data["q_per_phase"]
+    qs_42_with_visual_criteria = data["qs"]
+    qdots_42_with_visual_criteria = data["qdots"]
+    qddots_42_with_visual_criteria = data["qddots"]
+    time_parameters_42_with_visual_criteria = data["time_parameters"]
+    q_reintegrated_42_with_visual_criteria = data["q_reintegrated"]
+    qdot_reintegrated_42_with_visual_criteria = data["qdot_reintegrated"]
+    time_vector_42_with_visual_criteria = data["time_vector"]
+    interpolated_states_42_with_visual_criteria = data["interpolated_states"]
+
+with open(file_name_831, "rb") as f:
+    data = pickle.load(f)
+    q_per_phase_831 = data["q_per_phase"]
+    qs_831 = data["qs"]
+    qdots_831 = data["qdots"]
+    qddots_831 = data["qddots"]
+    time_parameters_831 = data["time_parameters"]
+    q_reintegrated_831 = data["q_reintegrated"]
+    qdot_reintegrated_831 = data["qdot_reintegrated"]
+    time_vector_831 = data["time_vector"]
+    interpolated_states_831 = data["interpolated_states"]
+
+with open(file_name_831_with_visual_criteria, "rb") as f:
+    data = pickle.load(f)
+    q_per_phase_831_with_visual_criteria = data["q_per_phase"]
+    qs_831_with_visual_criteria = data["qs"]
+    qdots_831_with_visual_criteria = data["qdots"]
+    qddots_831_with_visual_criteria = data["qddots"]
+    time_parameters_831_with_visual_criteria = data["time_parameters"]
+    q_reintegrated_831_with_visual_criteria = data["q_reintegrated"]
+    qdot_reintegrated_831_with_visual_criteria = data["qdot_reintegrated"]
+    time_vector_831_with_visual_criteria = data["time_vector"]
+    interpolated_states_831_with_visual_criteria = data["interpolated_states"]
 
 
-b = bioviz.Viz(biorbd_model_path)
-b.load_movement(qs)
+# ---------------------------------------- Animate comparison ---------------------------------------- #
+
+q_per_phase_42_combined = [np.vstack((q_per_phase_42_with_visual_criteria[i], q_per_phase_42[i])) for i in range(len(q_per_phase_42))]
+qs_42_combined = np.vstack((qs_42_with_visual_criteria, qs_42))
+# b = bioviz.Kinogram(model_path=biorbd_model_path_42_both,
+#                    mesh_opacity=0.8,
+#                    show_global_center_of_mass=False,
+#                    show_gravity_vector=False,
+#                    show_segments_center_of_mass=False,
+#                    show_global_ref_frame=False,
+#                    show_local_ref_frame=False,
+#                    experimental_markers_color=(1, 1, 1),
+#                    background_color=(1.0, 1.0, 1.0),
+#                     )
+# b.load_movement(q_per_phase_42_combined)
+# b.set_camera_focus_point(0, 0, 2.5)
+# b.set_camera_zoom(0.25)
+# b.exec(frame_step=20,
+#        save_path="Kinograms/42_both.svg")
+
+qs_42_combined_real_time = combine_real_time(interpolated_states_42_with_visual_criteria, interpolated_states_42)
+b = bioviz.Viz(biorbd_model_path_42_both,
+               mesh_opacity=0.8,
+               show_global_center_of_mass=False,
+               show_gravity_vector=False,
+               show_segments_center_of_mass=False,
+               show_global_ref_frame=False,
+               show_local_ref_frame=False,
+               experimental_markers_color=(1, 1, 1),
+               background_color=(1.0, 1.0, 1.0),
+               )
+b.load_movement(qs_42_combined_real_time)
+b.set_camera_zoom(0.25)
+b.set_camera_focus_point(0, 0, 2.5)
 b.exec()
 
+qs_42_combined_array = np.hstack((q_per_phase_42_combined[0][:, :-1], q_per_phase_42_combined[1]))
+qs_42_combined_array[15, :] -= 2
 
-# measure the value of the custom_trampoline_bed_in_peripheral_vision function
-def custom_trampoline_bed_in_peripheral_vision(model, q):
+b = bioviz.Viz(biorbd_model_path_42_both,
+               mesh_opacity=0.8,
+               show_global_center_of_mass=False,
+               show_gravity_vector=False,
+               show_segments_center_of_mass=False,
+               show_global_ref_frame=False,
+               show_local_ref_frame=False,
+               experimental_markers_color=(1, 1, 1),
+               background_color=(1.0, 1.0, 1.0),
+               )
+b.load_movement(qs_42_combined_array)
+b.set_camera_zoom(0.25)
+b.set_camera_focus_point(0, 0, 1.5) ##
+b.maximize()
+b.update()
+b.start_recording(f"comp_42.ogv")
+for frame in range(qs_42_combined_array.shape[1] + 1):
+    b.movement_slider[0].setValue(frame)
+    b.add_frame()
+b.stop_recording()
+b.quit()
 
-    a = 1.07  # Trampoline with/2
-    b = 2.14  # Trampoline length/2
-    n = 6  # order of the polynomial for the trampoline bed rectangle equation
+# Animate comparison
+q_per_phase_831_combined = [np.vstack((q_per_phase_831_with_visual_criteria[i], q_per_phase_831[i])) for i in range(len(q_per_phase_831))]
+qs_831_combined = np.vstack((qs_831_with_visual_criteria, qs_831))
+b = bioviz.Kinogram(model_path=biorbd_model_path_831_both,
+                   mesh_opacity=0.8,
+                   show_global_center_of_mass=False,
+                   show_gravity_vector=False,
+                   show_segments_center_of_mass=False,
+                   show_global_ref_frame=False,
+                   show_local_ref_frame=False,
+                   experimental_markers_color=(1, 1, 1),
+                   background_color=(1.0, 1.0, 1.0),
+                    )
+b.load_movement(q_per_phase_831_combined)
+b.set_camera_zoom(0.25)
+b.set_camera_focus_point(0, 0, 2.5)
+b.exec(frame_step=20,
+       save_path="Kinograms/831_both.svg")
 
-    # Get the gaze vector
-    eyes_vect_start_marker_idx = 6
-    eyes_vect_end_marker_idx = 7
-    gaze_vector = model.markers(q)[eyes_vect_end_marker_idx].to_array() - model.markers(q)[eyes_vect_start_marker_idx].to_array()
-
-    first_condition = 0
-    second_condition = 0
-    third_condition = 0
-    point_in_the_plane = np.array([1, 2, -0.83])
-    vector_normal_to_the_plane = np.array([0, 0, 1])
-    obj = 0
-    for i_r in range(11):
-        for i_th in range(10):
-
-            # Get this vector from the vision cone
-            marker_idx = 9 + i_r * 10 + i_th
-            vector_origin = model.markers(q)[eyes_vect_start_marker_idx].to_array()
-            vector_end = model.markers(q)[marker_idx].to_array()
-            vector = vector_end - vector_origin
-
-            # Get the intersection between the vector and the trampoline plane
-            t = (np.dot(point_in_the_plane, vector_normal_to_the_plane) - np.dot(vector_normal_to_the_plane, vector_origin)) / np.dot(
-                vector, vector_normal_to_the_plane
-            )
-            point_projection = vector_origin + vector * np.abs(t)
-
-            # Determine if the point is inside the trampoline bed
-            # Rectangle equation : (x/a)**n + (y/b)**n = 1
-            # The function is convoluted with tanh to make it:
-            # 1. Continuous
-            # 2. Not encourage to look to the middle of the trampoline bed
-            # 3. Largely penalized when outside the trampoline bed
-            # 4. Equaly penalized when looking upward
-            obj += np.tanh(((point_projection[0]/a)**n + (point_projection[1]/b)**n) - 1) + 1
-
-    if gaze_vector[2] > -0.01:
-        val = 2*10*11
-        first_condition = 1
-    elif np.abs(gaze_vector[0]/gaze_vector[2]) > np.tan(3*np.pi/8):
-        val = 2*10*11
-        second_condition = 1
-    elif np.abs(gaze_vector[1]/gaze_vector[2]) > np.tan(3*np.pi/8):
-        val = 2*10*11
-        third_condition = 1
-    else:
-        val = obj
-
-    return val, first_condition, second_condition, third_condition
-
-if WITH_VISUAL_CRITERIA:
-    model = biorbd.Model(biorbd_model_path)
-    num_frames = qs.shape[1]
-    obj = np.zeros(num_frames)
-    first_condition = np.zeros(num_frames)
-    second_condition = np.zeros(num_frames)
-    third_condition = np.zeros(num_frames)
-    for i in range(num_frames):
-        obj[i], first_condition[i], second_condition[i], third_condition[i] = custom_trampoline_bed_in_peripheral_vision(model, qs[:, i])
-
-    plt.figure()
-    plt.plot(obj, 'k')
-    plt.plot(first_condition * 220, 'r')
-    plt.plot(second_condition * 220, 'g')
-    plt.plot(third_condition * 220, 'b')
-    plt.show()
+qs_831_combined_real_time = combine_real_time(interpolated_states_831_with_visual_criteria, interpolated_states_831)
+b = bioviz.Viz(biorbd_model_path_831_both,
+               mesh_opacity=0.8,
+               show_global_center_of_mass=False,
+               show_gravity_vector=False,
+               show_segments_center_of_mass=False,
+               show_global_ref_frame=False,
+               show_local_ref_frame=False,
+               experimental_markers_color=(1, 1, 1),
+               background_color=(1.0, 1.0, 1.0),
+               )
+b.load_movement(qs_831_combined_real_time)
+b.set_camera_zoom(0.25)
+b.set_camera_focus_point(0, 0, 2.5)
+b.exec()
